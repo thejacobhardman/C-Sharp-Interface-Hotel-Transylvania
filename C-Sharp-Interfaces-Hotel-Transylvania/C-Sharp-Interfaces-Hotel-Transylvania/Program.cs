ebd02b5ea8f93e0cead2a8971eb469696cbde2d9
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-11-16 10:56:39 -0700

    create project

---
+++ C-Sharp-Interfaces-Hotel-Transylvania/C-Sharp-Interfaces-Hotel-Transylvania/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Sharp_Interfaces_Hotel_Transylvania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
